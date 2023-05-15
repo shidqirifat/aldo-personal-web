86ecbf8a88180e75a50f9229f9d664b89e08531b
Shidqi <rifatpangestu87@gmail.com>
2023-05-15 14:45:30 +0700

    feat: change link resume

--- data/CONFIG.js
+++ data/CONFIG.js
@@ -1,7 +1,7 @@
 export const GLOBAL_DATA = {
   email: 'raffialdo123@gmail.com',
   resume:
-    'https://drive.google.com/file/d/1BDd9OD8cN796UwFFN40icqRdnYNAdaeM/view?usp=share_link',
+    'https://drive.google.com/file/d/16viUjuCABPknfC4pe48jVYEpfSO6Ked1/view?usp=sharing',
   phone_number: '6281291142798',
   phone_number_with_space: '+62 812 9114 2798',
   instagram: 'https://www.instagram.com/raffialdo_/',
